85ec78dd56f02be1487f58ac5b315597950636e2
usman-siiddiqui123 <siddiquiusman44@gmail.com>
2025-03-18 23:04:27 +0500

    add code

---
+++ matchcase.py
@@ -0,0 +1,16 @@
+a = int(input("enter your number: "))
+
+match a:
+    case 1:
+        print("case is 1")
+    case 2:
+        print("case is 2")
+    case 3:
+        print("case is 3")
+    case 4:
+        print("case is 4")
+    case 5:
+        print("case is 5")
+    case _:
+        print("case is not found")
+    
